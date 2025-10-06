b2150c2cf0570d51055e64b6e473bc84c1474644
Philippe Beer <technology@phivana.com>
2025-10-07 00:32:31 +0200

    new

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
-// apps/000_phivana-sites/next.config.js
+// apps/renderer/next.config.js
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: [
-    "@phivana/auth",
     "@phivana/ui",
-    "@phivana/types",
-    "@phivana/db",
-    "@phivana/site-chrome",
-    "@phivana/schemas",
     "@phivana/site-builder",
     "@phivana/site-components",
+    "@phivana/schemas",
+    "@phivana/brand",
+    "@phivana/types",
   ],
   outputFileTracingRoot: path.join(__dirname, "../.."),
   webpack: (config) => {
