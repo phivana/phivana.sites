33578f81793f2376e7f35f04938eed1627a6cfba
Philippe Beer <technology@phivana.com>
2025-10-06 21:49:30 +0200

    new try

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
+// apps/000_phivana-sites/next.config.js
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@phivana/auth", "@phivana/ui", "@phivana/types", "@phivana/db", "@phivana/site-chrome", "@phivana/schemas", "@phivana/site-builder", "@phivana/site-components"],
-  typedRoutes: true,
+  transpilePackages: [
+    "@phivana/auth",
+    "@phivana/ui",
+    "@phivana/types",
+    "@phivana/db",
+    "@phivana/site-chrome",
+    "@phivana/schemas",
+    "@phivana/site-builder",
+    "@phivana/site-components",
+  ],
   outputFileTracingRoot: path.join(__dirname, "../.."),
-  // ...rest of your config
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react$: require.resolve("react"),
+      "react-dom$": require.resolve("react-dom"),
+    };
+    config.resolve.symlinks = true;
+    return config;
+  },
 };
